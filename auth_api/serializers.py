635a3e1c36e88ddac81f46f81ffdce2bc2a2f810
Chester Vincent <chetvincent@chesters-mbp-2.mynetworksettings.com>
2023-01-06 15:07:39 -0500

    changes

--- auth_api/serializers.py
+++ auth_api/serializers.py
@@ -9,12 +9,12 @@ from django.contrib.auth.hashers import make_password, check_password
 class UserAccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserAccount
-        fields = ('id', 'email', 'password')
+        fields = ('id', 'username', 'password')
 
     ### THIS HASHES A NEW USERS PASSWORD WHEN THEY CREATE AN ACCOUNT
     def create(self, validated_data):
         user = UserAccount.objects.create(
-        email=validated_data['email'],
+        username=validated_data['username'],
         password = make_password(validated_data['password'])
         )
         user.save()
@@ -22,7 +22,7 @@ class UserAccountSerializer(serializers.ModelSerializer):
 
     ### THIS MAKES SURE THEIR UPDATED PASSWORDS ARE ALSO HASHED
     def update(self,instance, validated_data):
-        user = UserAccount.objects.get(email=validated_data["email"])
+        user = UserAccount.objects.get(username=validated_data["username"])
         user.password = make_password(validated_data["password"])
         user.save()
         return user
