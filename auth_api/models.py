a902287ad53907dbe764f7182a7cf2f0fa5e0feb
Chester Vincent <chetvincent@chesters-mbp-2.mynetworksettings.com>
2023-01-06 11:18:35 -0500

    login

--- auth_api/models.py
+++ auth_api/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class UserAccount(models.Model):
-    email = models.CharField(max_length=75, unique=True)
+    username = models.CharField(max_length=75, unique=True)
     password = models.CharField(max_length=1000)
